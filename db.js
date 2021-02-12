2c188b3c6cf523a1e02feada17b66b61e0117d57
Jordan Steel <jordansteel88@gmail.com>
2021-02-12 13:02:28 -0600

    debug db.connect

--- db.js
+++ db.js
@@ -5,10 +5,18 @@
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
+console.log('***********getDatabaseUri***********');
+console.log(getDatabaseUri());
+console.log('***********getDatabaseUri***********');
+
+
 const db = new Client({
   connectionString: getDatabaseUri(),
 });
 
+console.log('********before db.connect()');
 db.connect();
+console.log('********after db.connect()');
+
 
 module.exports = db;
